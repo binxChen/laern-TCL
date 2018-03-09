8d3b6415d247159e0fa387c141fd644522c1f0f4
binxchen <xiaobina111@163.com>
2018-03-09 17:38:39 +0800

    1.1.0

--- src/js/main.js
+++ src/js/main.js
@@ -1,12 +1,15 @@
 var TCL = TCL ||
     {
+        RUNTIME:{
+            swiper:'',
+        },
         init:function () {
             this.initElement();
             this.initlayout();
             this.initEvent();
         },
         initElement:function () {
-
+            var R = TCL.RUNTIME
         },
         initlayout:function () {
             console.log($(window).height());
@@ -16,15 +19,38 @@ var TCL = TCL ||
             this.loadingSwiper();
         },
         loadingSwiper:function () {
-            var swiper = new Swiper('.swiper-container', {
+            var R = TCL.RUNTIME;
+             R.swiper = new Swiper('.swiper-container', {
                 direction: 'vertical',
                 pagination: {
                     el: '.swiper-pagination',
                     clickable: true,
                 },
-            });
+                mousewheel: true,
+                hashNavigation: {
+                    watchState: true,
+                },
+                on: {
+                }
+             })
+        },
+        //获取url的锚节点
+        getAnchor:function () {
+
+        },
+        //动画
+        animationTo: function (idx) {
+            switch (idx){
+                case '1' :
+                    break;
+                default:
+
         }
+
+
     };
+
 $(function () {
    TCL.init();
 });
+//todo: 1.动画2.鼠标3.底部栏4.小icon动画
